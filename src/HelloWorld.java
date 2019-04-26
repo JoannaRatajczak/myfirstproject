9fa5a7d2f2adead442ef99cfc019d2bac07f8a55
JoannaRatajczak <joanna.jr.ratajczak@gmail.com>
2019-04-26 19:30:28 +0200

    Dodanie pliku

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println(4);
+        System.out.println('o');
+    }
+
+}
