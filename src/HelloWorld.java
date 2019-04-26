9e06eb8b14644e6daf9c71c8c4549a9304ed4253
JoannaRatajczak <joanna.jr.ratajczak@gmail.com>
2019-04-26 19:41:08 +0200

    Dodanie pliku próba nr 3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hi ...");
         System.out.println(4);
-        System.out.println('o');
+        System.out.println('oooooo');
     }
 
 }
